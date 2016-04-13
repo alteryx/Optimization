33a4ade67edc1ac4e2026789b1f4cbc464107d3d
Chris Freeman <cfreeman.code@gmail.com>
2016-04-13 09:01:04 -0500

    Change the webpack config to generate UMD
    Webpack config now has branching for build steps, so it can either output a UMD module (better for Alteryx), or a full app build with separate vendor files and webpack chunking.
    
    The `npm run compile-ayx` script now builds UMD instead of cjs

--- App/webpack.config.js
+++ App/webpack.config.js
@@ -79,20 +79,8 @@ if (TARGET === 'start' || !TARGET) {
   });
 }
 
-if (TARGET === 'build') {
-  module.exports = merge(common, {
-    // devtool: 'source-map',
-    entry: {
-      // flag all `package.json` dependencies as vendor files
-      vendor: Object.keys(pkg.dependencies),
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].js',
-      // Create a hash for each file in the build so we can detect which files have changed
-      // filename: '[name].[chunkhash].js',
-      // chunkFilename: '[chunkhash].js',
-    },
+if (TARGET === 'build' || TARGET === 'build-umd') {
+  const prodConfig = {
     module: {
       loaders: [
         // Extract CSS during the build process
@@ -124,11 +112,6 @@ if (TARGET === 'build') {
       new CleanWebpackPlugin(PATHS.dist),
       // Output extracted CSS to its own file
       new ExtractTextPlugin('[name].css'),
-      // new ExtractTextPlugin('[name].[chunkhash].css'),
-      // Split dependencies into a `vendor` file and provide a manifest
-      new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest'],
-      }),
       // Use the `production` flag so we get full optimization from React when building
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"',
@@ -143,5 +126,55 @@ if (TARGET === 'build') {
       new webpack.optimize.DedupePlugin(),
       new webpack.optimize.OccurenceOrderPlugin(),
     ],
-  });
+  };
+
+  if (TARGET === 'build-umd') {
+    module.exports = merge(common, prodConfig, {
+      output: {
+        path: PATHS.dist,
+        libraryTarget: 'umd',
+        library: 'Optimization',
+        umdNamedDefine: 'optimization',
+      },
+      externals: {
+        react: {
+          commonjs: 'react',
+          amd: 'React',
+          root: 'React',
+        },
+        'react-dom': {
+          commonjs: 'react-dom',
+          amd: 'ReactDOM',
+          root: 'ReactDOM',
+        },
+        jquery: {
+          commonjs: 'jquery',
+          amd: 'jquery',
+          root: '$',
+        },
+      },
+    });
+  }
+
+  if (TARGET === 'build') {
+    module.exports = merge(common, prodConfig, {
+      // devtool: 'source-map',
+      entry: {
+        // flag all `package.json` dependencies as vendor files
+        vendor: Object.keys(pkg.dependencies),
+      },
+      output: {
+        path: PATHS.dist,
+        filename: '[name].js',
+        // Create a hash for each file in the build so we can detect which files have changed
+        // filename: '[name].[chunkhash].js',
+        // chunkFilename: '[chunkhash].js',
+      },
+      plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+          names: ['vendor', 'manifest'],
+        }),
+      ],
+    });
+  }
 }
