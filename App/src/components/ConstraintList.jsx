5e6ac3f6280f06ef6aad34b73bf9721938370f45
Chris Freeman <cfreeman.code@gmail.com>
2016-04-28 14:00:04 -0500

    Move `Editor` into `ConstraintList` component
    Now constraints and optimization both have their own unique editors.
    
    As a result, we're able to remove the Alteryx editor widget entirely.

--- App/src/components/ConstraintList.jsx
+++ App/src/components/ConstraintList.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { observer } from 'mobx-react';
-
 import ConstraintItem from './ConstraintItem';
+import Editor from './Editor';
 
 function ConstraintList({ store }) {
   const cs = store.constraintStore;
 
-  const handleAdd = () => {
+  const handleAdd = (e) => {
+    e.preventDefault();
     cs.addConstraint(store.editorValue);
-    // store.update();
-    store.updateEditor('');
   };
 
-  const handleSave = () => {
+  const handleSave = (e) => {
+    e.preventDefault();
     cs.currentConstraint.save(store.editorValue);
+  };
+
+  const handleChange = (v) => {
+    store.updateEditor(v);
   };
 
   const handleRemove = (e, c) => {
@@ -23,6 +27,7 @@ function ConstraintList({ store }) {
 
   return (
     <div>
+      <Editor value={store.editorValue} hintList={store.fieldNameArray} onChange={handleChange} />
       <button
         className="btn btn-default"
         onClick={store.saveOrAdd ? handleAdd : handleSave}
