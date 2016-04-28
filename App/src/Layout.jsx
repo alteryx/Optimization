913a03d4cf2258018acb6cfa9aaff1a1c5aafb7e
Chris Freeman <cfreeman.code@gmail.com>
2016-04-28 18:03:37 -0500

    Update `Layout`
    Using bootstrap grid layout to to add la little space between the different rows.
    
    Also moved `Fields` component to the first position in the tabbed portion

--- App/src/Layout.jsx
+++ App/src/Layout.jsx
@@ -8,7 +8,17 @@ import NavTabs from './components/NavTabs';
 
 function Layout({ store }) {
   return (
+    <div className="container">
+      <div className="row spacer-10">
         <FieldNameInput store={store} />
+      </div>
+      <div className="row">
+        <NavTabs selected={store.selectedTab} onSelect={store.updateSelectedTab}>
+          <FieldList label="Fields" store={store} />
+          <Objective label="Objective" store={store} />
+          <ConstraintList label="Constraints" store={store} />
+        </NavTabs>
+      </div>
     </div>
   );
 }
