c5944de878118f36dc52ac91d21f49eb6594c734
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-12 14:00:22 -0700

    update to new interactive reporting function.

--- Supporting_Macros/Optimization1.R
+++ Supporting_Macros/Optimization1.R
@@ -55,45 +55,5 @@ print(config)
 payload <- list(config = config, inputs = inputs)
 
 ## Interactive Visualization ----
-library(AlteryxRviz)
-iOutput <- function(s2a, varNames){
-  if (is.null(varNames)){
-    varNames = paste0("x", seq_along(s2a$solution))
-  }
-  d = data.frame(x = varNames, y = s2a$solution)
-  p4 = c3(
-    data = list(json = d, keys = list(x = 'x', value = list('y')), type = 'bar'),
-    axis = list(
-      rotated = TRUE,
-      x = list(type = "category", tick = list(outer = list(show = FALSE))),
-      y = list(label = '')
-    ),
-    grid = list(y = list(show = TRUE)),
-    tooltip = list(show = FALSE),
-    legend = list(show = FALSE),
-    width = '100%',
-    height = 300
-  )
-
-  objective_function = list(
-    Solution = list(
-      value = s2a$objval,
-      title = 'Optimal Value',
-      definition = "<p>This is the optimal value</p>"
-    )
-  )
-
-  myNavBar <- Navbar2("Optimization",
-    navList(navItem(icon('play'), 'Tour', href='#'))
-  )
-  app <- keen_dash(
-    myNavBar,
-    Row(
-      infobox(objective_function, div = 'col-xs-12 col-md-4'),
-      Panel(c(xs = 12, md = 8), p4, 'Optimal Solution', notes = 'Optimal Solution')
-    )
-  )
-  renderInComposer(app, nOutput = 3)
-}
-s2a <- AlteryxSolve(payload)
-iOutput(s2a, inputs$O$variable)
+out <- AlteryxSolve(payload)
+renderInComposer(makeInteractiveReport(out), nOutput = 3)
