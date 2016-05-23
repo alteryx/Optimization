b399cc852edf20cd8ca1bc791cc2b6336e6b6919
Chris Freeman <cfreeman.code@gmail.com>
2016-05-23 13:56:33 -0500

    Remove old comments and orphaned components
    Due to recent refactoring, several components are actually redundant and no longer being used, so I'm dropping them.

--- App/src/components/FieldListItem.jsx
+++ App/src/components/FieldListItem.jsx
@@ -1,7 +1,6 @@
 import React, { PropTypes as P } from 'react';
 import { observer } from 'mobx-react';
 import { observable, toJSON } from 'mobx';
-// import BoundItemContainer from './BoundItemContainer';
 import BoundItemEdit from './BoundItemEdit';
 import BoundItemView from './BoundItemView';
 import Select from './Select';
@@ -103,47 +102,3 @@ class FieldListItem extends React.Component {
 }
 
 export default FieldListItem;
-
-// function FieldListItem({ field, options }) {
-//   const handleDelete = () => field.delete();
-//   const handleEdit = (v) => field.update('bound', v);
-//   const onSelect = (v) => field.update('type', v);
-//
-//   return (
-//     <div className="list-group-item short-item">
-//       <div className="row">
-//         <div className="col-xs-8">
-//           <BoundItemContainer
-//             isEditing={this.isEditing}
-//             bound={field.bound}
-//             onDelete={handleDelete}
-//             onEdit={handleEdit}
-//           />
-//         </div>
-//         <div className="col-xs-4 full-width">
-//           <div className="input-group">
-//             <Select
-//               // style={{ width: 'auto' }}
-//               className="form-control"
-//               value={field.type}
-//               options={options}
-//               onChange={onSelect}
-//             />
-//               <span className="input-group-btn">
-//                 <button type="button" className="btn btn-xs btn-transparent" onClick={handleDelete}>
-//                   <i className="fa fa-trash text-danger"></i>
-//                 </button>
-//               </span>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-//
-// FieldListItem.propTypes = {
-//   field: P.object.isRequired,
-//   options: P.array.isRequired,
-// };
-//
-// export default observer(FieldListItem);
