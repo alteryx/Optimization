e11ed812a2ed6485443520c003f90ecaad9dc380
Chris Freeman <cfreeman.code@gmail.com>
2016-06-03 15:41:27 -0500

    remove leftover comments

--- App/src/components/SplashScreen.jsx
+++ App/src/components/SplashScreen.jsx
@@ -5,11 +5,9 @@ function SplashScreen() {
   return (
     <div className="panel panel-default">
       <div className="panel-body">
-        {/*<h4 className="text-center">*/}
         <p className="text-center panel-text">
           Enter variables as a comma-separated list in the text entry field above, then click Add to define the bounds and types.
         </p>
-        {/*</h4>*/}
       </div>
     </div>
   );
